87a92ae4cdf552b921158f8b76d359da2669fb4b
PathonScript <pathonscript@gmail.com>
2022-04-13 09:09:27 +0700

    grid system -^> image, border size and scalability

--- src/lib/components/Achievements.svelte
+++ src/lib/components/Achievements.svelte
@@ -2,18 +2,27 @@
 	import Projects from './Project.svelte';
 </script>
 
-<div class="grid h-screen place-items-center">
-	<h1 class="text-6xl">Portfolio</h1>
-	<span class="grid-rows-3">
-		<Projects
-			name="Bob"
-			image="image yeah"
-			description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu dolor velit. Nunc eu sollicitudin elit, ut venenatis eros. Morbi leo tortor, faucibus ut ligula sit amet, consectetur ornare orci. Suspendisse iaculis, diam in pharetra consectetur, justo urna porta lectus, eu rutrum ex nisi eu diam. Maecenas non rutrum leo, eget egestas nunc. Curabitur ultrices nunc id nisl scelerisque, eu sodales lacus scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-		/>
-		<Projects
-			name="John"
-			image="image yeah"
-			description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu dolor velit. Nunc eu sollicitudin elit, ut venenatis eros. Morbi leo tortor, faucibus ut ligula sit amet, consectetur ornare orci. Suspendisse iaculis, diam in pharetra consectetur, justo urna porta lectus, eu rutrum ex nisi eu diam. Maecenas non rutrum leo, eget egestas nunc. Curabitur ultrices nunc id nisl scelerisque, eu sodales lacus scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-		/>
-	</span>
+<h1 class="text-6xl text-center">Portfolio</h1>
+<div class="grid grid-cols-2 h-screen place-items-center">
+	<Projects
+		name="Bob"
+		image="image yeah"
+		description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu dolor velit. Nunc eu sollicitudin elit, ut venenatis eros. Morbi leo tortor, faucibus ut ligula sit amet, consectetur ornare orci. Suspendisse iaculis, diam in pharetra consectetur, justo urna porta lectus, eu rutrum ex nisi eu diam. Maecenas non rutrum leo, eget egestas nunc. Curabitur ultrices nunc id nisl scelerisque, eu sodales lacus scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+	/>
+	<Projects
+		name="John"
+		image="image yeah"
+		description="Lorem ipsum dolor  lis, diam in pharetra consectetur, justo urna porta lectus, eu rutrum ex nisi eu diam. Maecenas non rutrum leo, eget egestas nunc. Curabitur ultrices nunc id nisl scelerisque, eu sodales lacus scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+	/>
+
+	<Projects
+		name="John"
+		image="image yeah"
+		description="Lorem ipsum dolor  lis, diam in pharetra consectetur, justo urna porta lectus, eu rutrum ex nisi eu diam. Maecenas non rutrum leo, eget egestas nunc. Curabitur ultrices nunc id nisl scelerisque, eu sodales lacus scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+	/>
+	<Projects
+		name="John"
+		image="image yeah"
+		description="Lorem ipsum dolor  lis, diam in pharetra consectetur, justo urna porta lectus, eu rutrum ex nisi eu diam. Maecenas non rutrum leo, eget egestas nunc. Curabitur ultrices nunc id nisl scelerisque, eu sodales lacus scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+	/>
 </div>
--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -4,7 +4,7 @@
 	export let description;
 </script>
 
-<div class="border border-black text-center m-10 p-2 w-96">
+<div class="border border-black text-center m-10 p-2 w-96 ">
 	<h1 class="text-5xl">{name}</h1>
 	<h1 class="m-4">{image}</h1>
 	<p class="m-4 text-base">{description}</p>
